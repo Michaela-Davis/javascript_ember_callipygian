6df8de8bfb41ec7833b5e61c0c71f3227f19ba6d
Erica Wright and Michaela Davis <erica@>
2017-03-22 11:08:42 -0700

    add styling to post links

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('create');
+  this.route('update');
 });
 
 export default Router;
