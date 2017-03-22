52d89c7e7bff46fbab4c56924f3d667561376c45
Erica Wright and Michaela Davis <michaela@>
2017-03-22 11:44:34 -0700

    add create route to make new post

---
+++ app/components/post-create.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    savePost1() {
+      var params = {
+        title: this.get('title'),
+        username: this.get('username'),
+        caption: this.get('caption'),
+        imageURL: this.get('imageURL')
+      }
+      this.sendAction('savePost2', params);
+    }
+  }
+});
--- app/routes/create.js
+++ app/routes/create.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    savePost3(params) {
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('index');
+    }
+
+  }
 });
